c6fe3fdc94d7d304eb7a30d667588b4bd1502347
Philipp Potisk <philipp.potisk@danubetech.com>
2019-09-25 14:26:07 +0200

    Path issue

--- actions/deploy-eks/entrypoint.sh
+++ actions/deploy-eks/entrypoint.sh
@@ -7,18 +7,24 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-IS_DEPLOYED=$(kubectl get deployments |grep ${DRIVER_NAME})
+#IS_DEPLOYED=$(kubectl get deployments |grep ${DRIVER_NAME})
 
 echo ${DRIVER_PATH}
-cd ${DRIVER_PATH}
+cd ./drivers/btcr
+
 ls -al
 
-if [ -z "${IS_DEPLOYED}" ]
-then
-   echo "Creating cluster (deployment-${DRIVER_NAME}.yml) ..."
-   #sh -c "kubectl create -f deployment-${DRIVER_NAME}.yml"
-   #sh -c "kubectl create -f nodeport-${DRIVER_NAME}.yml"
-else
-   echo "Updating cluster ..."
-   #sh -c "kubectl rollout restart -f deployment-${DRIVER_NAME}.yml"
-fi
+echo "Creating cluster (deployment-${DRIVER_NAME}.yml) ..."
+kubectl create -f deployment-${DRIVER_NAME}.yml
+kubectl create -f nodeport-${DRIVER_NAME}.yml
+
+
+#if [ -z "${IS_DEPLOYED}" ]
+#then
+#   echo "Creating cluster (deployment-${DRIVER_NAME}.yml) ..."
+#sh -c "kubectl create -f deployment-${DRIVER_NAME}.yml"
+#sh -c "kubectl create -f nodeport-${DRIVER_NAME}.yml"
+#else
+#   echo "Updating cluster ..."
+#   sh -c "kubectl rollout restart -f deployment-${DRIVER_NAME}.yml"
+#fi
