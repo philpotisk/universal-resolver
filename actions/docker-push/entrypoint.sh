b820b718024071ec2d1f1cbc6d66b52d8f07d130
Philipp Potisk <philipp.potisk@danubetech.com>
2019-09-25 12:48:18 +0200

    Fixed env-vars

--- actions/docker-push/entrypoint.sh
+++ actions/docker-push/entrypoint.sh
@@ -6,13 +6,20 @@ echo "path ${pwd}"
 
 ls -al
 
+echo "no env"
+echo ${CONTAINER_TAG};
+echo "env"
+echo ${env.CONTAINER_TAG};
+
 if [ -n "${BUILD_PATH}" ]
 then
    cd ${BUILD_PATH}
 fi
 
-docker build . -t ${CONTAINER_TAG}
+ls -al
+
+docker build . -t ${env.CONTAINER_TAG}
 
 docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
-docker push ${CONTAINER_TAG}
+docker push ${env.CONTAINER_TAG}
