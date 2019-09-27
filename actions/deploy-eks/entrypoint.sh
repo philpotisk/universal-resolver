87d3563dd8e79575c3cc06d3a6e51e41d0b541ac
Philipp Potisk <philipp.potisk@danubetech.com>
2019-09-27 10:53:23 +0200

    as

--- actions/deploy-eks/entrypoint.sh
+++ actions/deploy-eks/entrypoint.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-echo "EKS Deloyment of ${DRIVER_NAME}"
+echo "EKS Deloyment of ${CONTAINER_NAME}"
 
 set -e
 
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-#IS_DEPLOYED=$(kubectl get deployments |grep ${DRIVER_NAME})
+#IS_DEPLOYED=$(kubectl get deployments |grep ${CONTAINER_NAME})
 
-echo ${DRIVER_PATH}
-cd ${DRIVER_PATH}
+echo ${CONTAINER_PATH}
+cd ${CONTAINER_PATH}
 
 ls -al
 
-echo "Creating cluster (deployment-${DRIVER_NAME}.yml) ..."
-kubectl create -f deployment-${DRIVER_NAME}.yml
-kubectl create -f nodeport-${DRIVER_NAME}.yml
+echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
+kubectl create -f deployment-${CONTAINER_NAME}.yml
+kubectl create -f nodeport-${CONTAINER_NAME}.yml
 
 
 #if [ -z "${IS_DEPLOYED}" ]
 #then
-#   echo "Creating cluster (deployment-${DRIVER_NAME}.yml) ..."
-#sh -c "kubectl create -f deployment-${DRIVER_NAME}.yml"
-#sh -c "kubectl create -f nodeport-${DRIVER_NAME}.yml"
+#   echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
+#sh -c "kubectl create -f deployment-${CONTAINER_NAME}.yml"
+#sh -c "kubectl create -f nodeport-${CONTAINER_NAME}.yml"
 #else
 #   echo "Updating cluster ..."
-#   sh -c "kubectl rollout restart -f deployment-${DRIVER_NAME}.yml"
+#   sh -c "kubectl rollout restart -f deployment-${CONTAINER_NAME}.yml"
 #fi
